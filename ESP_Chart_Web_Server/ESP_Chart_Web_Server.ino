c66e8fa6183d699eab0ae77e9fde34c3a67d338f
Amiya Karmakar <79296886+Amiya04@users.noreply.github.com>
2023-06-01 13:13:20 +0530

    Add files via upload

---
+++ ESP_Chart_Web_Server/ESP_Chart_Web_Server.ino
@@ -0,0 +1,111 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+
+/*This Code is modified for BMP280 sensor. 
+                                     -- Amiya*/
+
+
+#include <ESPAsyncWebSrv.h>
+#include <WiFi.h>
+#include <SPIFFS.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+#include <SPI.h>
+#define BMP_SCK  (13)
+#define BMP_MISO (12)
+#define BMP_MOSI (11)
+#define BMP_CS   (10)
+
+Adafruit_BMP280 bmp; // I2C
+//Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
+//Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
+
+// Replace with your network credentials
+const char* ssid = "Pixel 5";
+const char* password = "abcd1234";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+String readBMP280Temperature() {
+  // Read temperature as Celsius (the default)
+  float t = bmp.readTemperature();
+  // Convert temperature to Fahrenheit
+  //t = 1.8 * t + 32;
+  if (isnan(t)) {    
+    Serial.println("Failed to read from BMP280 sensor!");
+    return "";
+  }
+  else {
+    Serial.println(t);
+    return String(t);
+  }
+}
+String readBMP280Pressure() {
+  float p = bmp.readPressure() / 100.0F;
+  if (isnan(p)) {
+    Serial.println("Failed to read from BMP280 sensor!");
+    return "";
+  }
+  else {
+    Serial.println(p);
+    return String(p);
+  }
+}
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  
+  bool status; 
+  // default settings
+  // (you can also pass in a Wire library object like &Wire2)
+  status = bmp.begin(0x76);  
+  if (!status) {
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    while (1);
+  }
+
+  // Initialize SPIFFS
+  if(!SPIFFS.begin()){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html");
+  });
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readBMP280Temperature().c_str());
+  });
+  server.on("/pressure", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readBMP280Pressure().c_str());
+  });
+
+  // Start server
+  server.begin();
+}
+ 
+void loop(){
+  
+}
